3b3ceb00901a5d67652dc9479b1fb931abb6aa7c
Manish Sharma <manidatzme@outlook.com>
2021-04-28 16:38:48 +0530

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM ubuntu
+maintainer manish
+RUN apt-get update && RUN apt-get install python3 -y
+ADD abc.py /var
+CMD python3 /var/abc.py 
---
+++ abc.py
@@ -0,0 +1,7 @@
+print("Welcome to the python world")
+a = 10
+b = 20
+c = a * b
+d = b + c
+print("The value of c is ", c)
+print("The value of d is ", d)
